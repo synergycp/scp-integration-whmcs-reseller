568f07244fffbfea21ef6d2676348bb7459a49dd
Zane Hooper <zanehoop@gmail.com>
2021-03-19 02:14:43 -0400

    build scripts

--- bin/export.sh
+++ bin/export.sh
@@ -4,4 +4,4 @@ bin/docker-exec rm -rf vendor Scp/vendor || exit $?
 bin/docker-exec composer install --no-dev || exit $?
 bin/docker-exec bash -c "cd Scp; composer install --no-dev" || exit $?
 OUTFILE=/scp/install.synergycp.com/bm/integration/whmcs/synergycpreseller.zip
-exec zip -r "$OUTFILE" . -x ".git*" ".idea/*" "bin/*" ".composer/*"
+exec zip -r "$OUTFILE" . -x ".git*" ".idea/*" "bin/*" ".composer/*" "Scp/.composer/*"
